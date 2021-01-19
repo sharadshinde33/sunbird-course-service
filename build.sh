9be031895f7f1316c41e511a15e2fb17ba16c204
sharadshinde33 <49449082+sharadshinde33@users.noreply.github.com>
2021-01-19 16:26:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,4 @@ org=$3
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
 echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
+echo "Build successfully ****"
